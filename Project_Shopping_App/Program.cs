85efdbb8bbf4c79698e28582f4f1cc57e8304cf2
hashem78 <hashem.olayano@gmail.com>
2020-04-11 17:19:25 +0300

    Added basic User and Product management

--- Project_Shopping_App/Program.cs
+++ Project_Shopping_App/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project_Shopping_App
+namespace shoppingApp
 {
     class Program
     {
+        public static void AddProduct(User usr,Product P)
+        {
+            usr.AddProduct(P);
+        }
         static void Main(string[] args)
         {
+            Admin admn = new Admin("admin", "00");
+            Customer usr = new Customer("Ahmad", "11");
         }
     }
 }
--- Project_Shopping_App/Properties/AssemblyInfo.cs
+++ Project_Shopping_App/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Project_Shopping_App")]
+[assembly: AssemblyTitle("shoppingApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Project_Shopping_App")]
+[assembly: AssemblyProduct("shoppingApp")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
